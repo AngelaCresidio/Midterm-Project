9fa956fd55227c701d3f37570fc9f3fba693cf7a
AngelaCresidio <85145531+AngelaCresidio@users.noreply.github.com>
2022-10-17 13:57:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const read = require  ('./read')
+const add = require ('./add')
+const del = require('./del')
+
+// get users input
+
+
+const cmd = process.argv
+
+
+if (cmd [2] === "add" ) {//add
+//build the object}
+
+ const note = {
+    "id" :cmd[3] ,
+    "title" : cmd [4] ,
+    "body" :cmd [5]
+}
+
+//get note.txt contents =
+
+const oldNote = read()
+
+//add new note to note.txt
+ 
+add(note, oldNote) 
+
+}
+
+if (cmd[2]=== 'read') { //read
+
+console.log(read())
+}
+
+if ( cmd [2] === "delete") {
+
+    const id = cmd[3]
+    const oldNote = read()
+
+    //delete an item from note array
+
+    del(id,oldNote)
+
+    console.log(read())
+}
